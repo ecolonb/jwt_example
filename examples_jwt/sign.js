43334e5f27057ed4cc6f622cdff42e15a9f53f70
Edgar Colón Barrientos <edgar@numina.mx>
2019-12-17 10:02:31 -0600

    Se agrega certificate and privatekey validos

--- examples_jwt/sign.js
+++ examples_jwt/sign.js
@@ -3,7 +3,7 @@ const {
     writeFile
 } = require('fs');
 const path = require('path')
-    // Libreria para firmar y hacer decode del token
+// Libreria para firmar y hacer decode del token
 const jwt = require('jsonwebtoken')
 
 // Obteniendo llaves para firmar el token
@@ -26,9 +26,11 @@ console.log('Token: ', token);
 
 //Guardando tokenString en token.json
 const tokenPath = path.join(__dirname, '/token.json');
-writeFile(tokenPath, JSON.stringify({
-    token: token
-}), (err) => {
+const tokenString = JSON.stringify({
+    token
+})
+
+writeFile(tokenPath, tokenString, (err) => {
     if (err) throw err;
     console.log('File with token was created!')
 })
